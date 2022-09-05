d618035cca01f024932289926269c0b142ba086d
unknown <jens.osterberg@gmail.com>
2022-09-05 16:35:48 +0300

    Updated package to include pg

--- knexfile.js
+++ knexfile.js
@@ -15,36 +15,16 @@ module.exports = {
     
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+  production : {
+    client : 'pg',
+    connection : process.env.DATABASE_URL,
+    pool : {
+      min : 2,
+      max : 10
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+    migrations : {
+      tablename : 'knex_migrations',
+      directory: './migrations'
     }
   }
-
 };
