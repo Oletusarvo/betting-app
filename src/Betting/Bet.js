92d4897002332eac0761dacbde6bfb070d1746eb
unknown <jens.osterberg@gmail.com>
2022-10-11 16:13:07 +0300

    Moved fold-function into Bet.js

--- src/Betting/Bet.js
+++ src/Betting/Bet.js
@@ -7,14 +7,37 @@ function Bet(){
     const {user, token, currency} = useContext(AppContext);
     const {setGameState, bet, game} = useContext(GameContext);
 
-
+    function fold(){
+        const req = new XMLHttpRequest();
+        req.open('POST', '/bets/fold', true);
+        req.setRequestHeader('Content-Type', 'application/json');
+        req.setRequestHeader('auth', token);
+    
+        const data = {
+            id : game.id,
+            username : bet.username,
+        }
+    
+        req.send(JSON.stringify(data));
+    
+        req.onload = () => {
+            if(req.status === 200){
+                const bet = JSON.parse(req.response);
+                const newState = {
+                    game, bet
+                };
+                setGameState(newState);
+            }
+        }
+    }
+    
     if(bet){
         if(bet.folded){
             return <span>Folded</span>
         }
         else{
             const foldingEnabled = bet.amount == game.minimum_bet;
-            return <span className="table-field">{`\"${bet.side}\" for ${currency + bet.amount.toLocaleString('en')}`} <button style={{width: "50px"}}disabled={foldingEnabled} onClick={() => fold(game, bet, token, setGameState)}>Fold</button></span>
+            return <span className="table-field">{`\"${bet.side}\" for ${currency + bet.amount.toLocaleString('en')}`} <button style={{width: "50px"}}disabled={foldingEnabled} onClick={fold}>Fold</button></span>
         }
     }
     else{
