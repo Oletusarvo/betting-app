95dd30b3e634201c85cacd5768844d2064db7a48
unknown <jens.osterberg@gmail.com>
2022-11-26 00:05:20 +0200

    Changed currency default symbol to be an empty string

--- src/currency.js
+++ src/currency.js
@@ -3,7 +3,7 @@ class Currency{
         this.precision = options.precision || 2;
         this.name = options.name || 'Default';
         this.short_name = options.short_name || 'DEF';
-        this.symbol = options.symbol || 'D';
+        this.symbol = options.symbol || '';
     }
 
     getString(amount){
