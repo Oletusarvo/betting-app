ba341ce1b7afc0705363bba50ea6e1dcbfb8f43c
unknown <jens.osterberg@gmail.com>
2022-11-30 22:27:34 +0200

    Updated home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -22,42 +22,28 @@ function Home(){
                         ja salasana. <br/>Sovelluksella ei ole mitään tekemistä veikkaus-rahapeliyhtiön kanssa. <br/><br/>
                         </strong>
                         
-                        Tervetuloa veikkaus-sovellukseen! Täällä voit luoda vetoja ja panostaa nopiksi kutsuttua virtuaalirahaa.
+                        Tervetuloa veikkaus-sovellukseen! Täällä voit luoda vetoja ja panostaa nopiksi kutsuttua virtuaalirahaa, sekä seurata muita käyttäjiä.
                         Jokaiselle uudelle tilille annetaan 100 noppaa käytettäväksi (Saattaa muuttua ilmoituksetta).
                         Kirjaudu sisään käyttäjänimellä ja salasanalla <strong>demo</strong> jos et toistaiseksi halua luoda tiliä (Vetoihin osallistuminen estetty).
                     </p>
                     <br/>
-                    <h2>Vedot</h2>
-                    <br/>
-                    <h3>Tyypit</h3>
-                    <p>
-                        Vetoja voidaan luoda kahdenlaisia:
-                    </p>
-                    <br/>
-                    <p>
-                        <strong>Boolean</strong> vedoilla on kaksi mahdollista lopputulosta: Kyllä tai Ei.<br/><br/>
-                        <strong>Multi-Choice</strong> vedoilla lopputulos määräytyy käyttäjän itse luomasta ja määrittämästä listasta.<br/><br/>
-                    </p>
-                    <br/>
-
+                    
                     <h2>Sovellus</h2>
+                    <br/>
                     <p>
-                        Sovelluksessa on neljä välilehteä:<br/><br/>
-                        <h3>Koti</h3>
-                        Tästä välilehdestä näet ja suljet omat vetosi. (Vaatii sisäänkirjautumisen).<br/><br/>
-                        <h3>Vedot</h3>
-                        Sisältää kaikkien käyttäjien luomat vedot. Voit kotisivun lisäksi sulkea omat vetosi myös täältä. Osallistu haluamaasi vetoon napauttamalla sen otsikkoa ja sitten auennutta yleisnäkymää.<br/><br/>
-                        <h3>Uusi veto</h3>
-                        Vetoideasi luodaan täältä.<br/><br/>
-                        <h3>Noppageneraattori</h3>
-                        Luo uusia noppia käytettäväksi kun taskut ovat tyhjät.<br/><br/>
-                        
+                        Sovelluksessa on kolme pääosiota: Seinä, Profiili ja Top-20 vedon lista.<br/>
+                        <h3>Seinä</h3><br/>
+                        Täältä näet ja osallistut seuraamiesi käyttäjien luomiin vetoihin.<br/><br/>
+                        <h3>Profiili</h3>
+                        Sisältää vain omat vetosi. Voit hallita niitä helposti täältä. Näet myös seuraajiesi, seurattujesi sekä luomiesi vetojen kokonaismäärän.<br/><br/>
+                        <h3>Top-20 lista</h3>
+                        Sisältää 20 suurimman potin omaavaa vetoa. Voit myös etsiä nimellä tiettyä vetoa joka ei välttämättä ole top 20 joukossa.
                     </p>
                     <br/>
-                
                     <p>
-                        Sovellus on tuotantovaiheessa ja saattaa sisältää virheitä. Jotkut osiot voivat myös näyttää kömpelöiltä, toistaiseksi.<br/>
-                        Ole hyvä ja ilmoita löytämäsi virheet, tai kirjoita kysymyksesi osoitteeseen <a href="mailto:nistikemisti@gmail.com"><i>nistikemisti@gmail.com.</i></a><br/><br/>
+                        Sovellus on tuotantovaiheessa ja saattaa sisältää vikoja. Jotkut osiot voivat myös näyttää kömpelöiltä, toistaiseksi.<br/>
+                        Ilmoitukset eivät toimi vielä kunnolla, mutta ovat työn alla.<br/>
+                        Ole hyvä ja ilmoita löytämäsi viat, tai kirjoita kysymyksesi tai ehdotuksesi osoitteeseen <a href="mailto:nistikemisti@gmail.com"><i>nistikemisti@gmail.com.</i></a><br/><br/>
                         Hauskoja skabahetkiä!
                     </p>
                 </div>
